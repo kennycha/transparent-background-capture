abb6c3b25d249741e9c46bc225a65fc1f8522d23
kennycha <dudqn136@naver.com>
2023-12-22 17:06:54 +0900

    feat: fabric canvas 추가

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -1,5 +1,6 @@
 import classNames from "classnames/bind";
 import styles from "./index.module.scss";
+import Canvas from "../Canvas";
 
 const ASSETS_BASE_URL = "/transparent-background-capture/assets";
 
@@ -9,7 +10,9 @@ const App = () => {
   return (
     <div className={cx("container")}>
       <img src={`${ASSETS_BASE_URL}/background.jpeg`} alt="background" />
-      <img src={`${ASSETS_BASE_URL}/sample.png`} alt="sample" />
+      <div className={cx("canvasWrapper")}>
+        <Canvas />
+      </div>
       <div className={cx("separator")} />
       <div className={cx("combined")}>
         <img
--- src/components/Canvas/index.tsx
+++ src/components/Canvas/index.tsx
@@ -1,10 +1,35 @@
 import classNames from "classnames/bind";
 import styles from "./index.module.scss";
+import { fabric } from "fabric";
+import { useEffect, useRef } from "react";
 
 const cx = classNames.bind(styles);
 
 const Canvas = () => {
-  return <canvas className={cx("container")}></canvas>;
+  const canvasElementRef = useRef<HTMLCanvasElement>(null);
+
+  useEffect(() => {
+    const canvasElement = canvasElementRef.current;
+
+    if (canvasElement) {
+      const canvas = new fabric.Canvas(canvasElement, {
+        width: 350,
+        height: 300,
+      });
+      console.log("fabric canvas engine created...");
+
+      return () => {
+        canvas.dispose();
+        console.log("fabric canvas engine disposed...");
+      };
+    }
+  }, []);
+
+  return (
+    <div className={cx("container")}>
+      <canvas ref={canvasElementRef} />
+    </div>
+  );
 };
 
 export default Canvas;
