e7a72045e4ce29d277866a8e6b8ff6df917b57bd
kennycha <dudqn136@naver.com>
2023-12-27 16:03:26 +0900

    feat: canvas에서 투명배경 이미지 뽑아서 겹쳐서 보여주기

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -1,18 +1,41 @@
 import classNames from "classnames/bind";
 import styles from "./index.module.scss";
 import Canvas from "../Canvas";
+import { useCallback, useState } from "react";
 
 const ASSETS_BASE_URL = "/transparent-background-capture/assets";
 
 const cx = classNames.bind(styles);
 
 const App = () => {
+  const [fabricCanvas, setFabricCanvas] = useState<fabric.Canvas>();
+  const [resultImageUrl, setResultImageUrl] = useState<string>();
+
+  const initializeFabricCanvas = useCallback((canvas: fabric.Canvas) => {
+    setFabricCanvas(canvas);
+  }, []);
+
+  const handleExtractButtonClick = async () => {
+    if (!fabricCanvas) return;
+
+    const sourceUrl = fabricCanvas.toDataURL({
+      format: "png",
+      left: 0,
+      top: 0,
+      width: 350,
+      height: 300,
+    });
+    setResultImageUrl(sourceUrl);
+  };
+
   return (
     <div className={cx("container")}>
       <img src={`${ASSETS_BASE_URL}/background.jpeg`} alt="background" />
       <div className={cx("canvasWrapper")}>
-        <Canvas />
+        <Canvas initializeFabricCanvas={initializeFabricCanvas} />
       </div>
+      <div className={cx("separator")} />
+      <img alt="result" src={resultImageUrl} />
       <div className={cx("separator")} />
       <div className={cx("combined")}>
         <img
@@ -20,11 +43,15 @@ const App = () => {
           src={`${ASSETS_BASE_URL}/background.jpeg`}
           alt="background"
         />
-        <img
-          className={cx("sample")}
-          src={`${ASSETS_BASE_URL}/sample.png`}
-          alt="sample"
-        />
+        <img className={cx("sample")} src={resultImageUrl} alt="sample" />
+      </div>
+      <div className={cx("controls")}>
+        <button
+          className={cx("extractButton")}
+          onClick={handleExtractButtonClick}
+        >
+          Extract
+        </button>
       </div>
     </div>
   );
--- src/components/Canvas/index.tsx
+++ src/components/Canvas/index.tsx
@@ -3,9 +3,13 @@ import styles from "./index.module.scss";
 import { fabric } from "fabric";
 import { useEffect, useRef } from "react";
 
+type Props = {
+  initializeFabricCanvas: (canvas: fabric.Canvas) => void;
+};
+
 const cx = classNames.bind(styles);
 
-const Canvas = () => {
+const Canvas = ({ initializeFabricCanvas }: Props) => {
   const canvasElementRef = useRef<HTMLCanvasElement>(null);
 
   useEffect(() => {
@@ -16,14 +20,37 @@ const Canvas = () => {
         width: 350,
         height: 300,
       });
+      initializeFabricCanvas(canvas);
       console.log("fabric canvas engine created...");
+
+      const line = new fabric.Line([10, 10, 100, 100], {
+        stroke: "red",
+        strokeWidth: 2,
+      });
+      const circle = new fabric.Circle({
+        radius: 30,
+        fill: "green",
+        left: 150,
+        top: 50,
+      });
+      const rect = new fabric.Rect({
+        fill: "blue",
+        width: 100,
+        height: 100,
+        left: 200,
+        top: 150,
+      });
+
+      canvas.add(line);
+      canvas.add(circle);
+      canvas.add(rect);
 
       return () => {
         canvas.dispose();
         console.log("fabric canvas engine disposed...");
       };
     }
-  }, []);
+  }, [initializeFabricCanvas]);
 
   return (
     <div className={cx("container")}>
